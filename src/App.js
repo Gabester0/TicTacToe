c4240cdf13ec440abef747396cfaaef1f017bab1
Gabester0 <gceipper@gmail.com>
2020-08-08 17:44:02 -0400

    working board

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 import Square from './Square';
 import './App.css';
 
 function App() {
 
+  const [player, setPlayer] = useState("X")
 
-  const squares = [...Array(9)].map( (e, i)=>  <Square id={i} key={i} value={i} ></Square> )
+  const squares = [...Array(9)].map( (e, i)=>  <Square id={i} key={i} number={i + 1} value={``} ></Square> )
 
   return (
     <div className='App'>
       <h1>Tic Tac Toe Game!</h1>
+      <h5>Current Player: {player}</h5>
       <Board>
         {squares}
       </Board>
--- src/Square.js
+++ src/Square.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Square.css';
 
 const Square = (props)=>{
 
     return  (
-        <div className="square">
+        <div className="square" id={props.id}>
+            <p>{props.number}</p>
             <h1>{props.value}</h1>
         </div>
     )
