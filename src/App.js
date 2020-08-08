d342867794cfedecf030380466b0ac03dc5d5b9b
Gabester0 <gceipper@gmail.com>
2020-08-08 17:50:51 -0400

    added board and winner state

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ import './App.css';
 
 function App() {
 
-  const [player, setPlayer] = useState("X")
+  const [player, setPlayer] = useState("X");
+  const [board, setBoard] = useState({ 
+    "0" : null,
+    "1" : null,
+    "2" : null,
+    "3" : null,
+    "4" : null,
+    "5" : null,
+    "6" : null,
+    "7" : null,
+    "8" : null,
+  })
+  const [winner, setWinner] = useState(false);
 
-  const squares = [...Array(9)].map( (e, i)=>  <Square id={i} key={i} number={i + 1} value={``} ></Square> )
+  const squares = [...Array(9)].map( (e, i)=>  <Square id={i} key={i} number={i + 1} value={board[i]} ></Square> )
 
   return (
     <div className='App'>
