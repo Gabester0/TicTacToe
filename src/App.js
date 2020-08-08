3e06b91de99b552c09f3c36cac2be86d8685204d
Gabester0 <gceipper@gmail.com>
2020-08-08 17:33:10 -0400

    brokenProps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Board from './Board';
+import Square from './Square';
 import './App.css';
 
 function App() {
+
+
+  const squares = [...Array(9)].map( (e, i)=>  <Square id={i} key={i} value={i} ></Square> )
+
   return (
     <div className='App'>
       <h1>Tic Tac Toe Game!</h1>
-      <Board />
+      <Board>
+        {squares}
+      </Board>
     </div>
   );
 }
---
+++ src/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Square = ()=>{
+
+    return  (
+        <div className="square">
+            <h1>{props.value}</h1>
+        </div>
+    )
+}
+
+export default Square;
