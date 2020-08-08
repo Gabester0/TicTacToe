71670d44d69e4cd03a892c6a9132fea109c554bf
Gabester0 <gceipper@gmail.com>
2020-08-08 17:10:34 -0400

    empty board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Board from './Board';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Tic Tac Toe</h1>
+      <h1>Tic Tac Toe Game!</h1>
+      <Board />
     </div>
   );
 }
