dddafd86f3e7614853b99ae053391936eb95c21a
Gabester0 <gceipper@gmail.com>
2020-08-23 02:14:12 -0400

    added build directory

---
+++ build/precache-manifest.9b2ce1b930c5030b73055e84819d7fee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea4946a571847fe4692897273f7b3e4",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "465b8e71f978ddbf0562",
+    "url": "/TicTacToe/static/css/main.12ce66dd.chunk.css"
+  },
+  {
+    "revision": "f1e226aac1432d794832",
+    "url": "/TicTacToe/static/js/2.c133f60b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TicTacToe/static/js/2.c133f60b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465b8e71f978ddbf0562",
+    "url": "/TicTacToe/static/js/main.92e36b24.chunk.js"
+  },
+  {
+    "revision": "43541b8edbb023f9c87b",
+    "url": "/TicTacToe/static/js/runtime-main.71adabef.js"
+  }
+]);
