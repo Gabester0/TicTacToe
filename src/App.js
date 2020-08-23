c60931ce57c057efad8f6fc661dedf2a4d6f94fd
Gabester0 <gceipper@gmail.com>
2020-08-22 22:55:18 -0400

    removed alert for draw and set message to show on screen isntead

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [xmoves, setXMoves] = useState([]);
   const [omoves, setOMoves] = useState([]);
   const [winner, setWinner] = useState(false);
+  const [draw, setDraw] = useState(false);
   const [lastWin, setLastWin] = useState([]);
 
   const solutions = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
@@ -29,19 +30,21 @@ function App() {
 
   useEffect( ()=>{
     let win;
-    if(player === "X"){
-      win = checkWinner(xmoves)  
-    } 
-    if(player === "O"){
-      win = checkWinner(omoves);
-    }
+    (player === "X") ? (win = checkWinner(xmoves)) : (win = checkWinner(omoves))
+    // if(player === "X"){
+    //   win = checkWinner(xmoves)  
+    // } 
+    // if(player === "O"){
+    //   win = checkWinner(omoves);
+    // }
     if(win){
       setWinner(true);
       alert(`Player ${player} is the winner!`)
       return
     }
     if(xmoves.length + omoves.length === 9){
-      handleDraw();
+      setDraw(true);
+      console.log(draw)
       return
     }
     setPlayer( (player) => player === "X" ? "O" : "X")
@@ -73,9 +76,6 @@ function App() {
     return false;
   }
 
-  const handleDraw = ()=>{
-    alert(`It's a draw.  No moves left`)
-  }
 
   const resetBoard = () => {
     //First Turn
@@ -84,6 +84,7 @@ function App() {
     setXMoves([]);
     setOMoves([]);
     setWinner(false);
+    setDraw(false);
     resetHighlight(lastWin);
     setLastWin([]);
   }
@@ -102,7 +103,7 @@ function App() {
     <div className='App'>
       <h1>Tic Tac Toe Game!</h1>
       <h5 style={winner ? {fontSize: 50 + `px`} : null}>
-        {!winner ? `Current Player: ${player}` : `Player ${player} is the winner!`}
+        {  draw ? `The game is a draw, please restart` : !winner ? `Current Player: ${player}` : `Player ${player} is the winner!`}
       </h5>
       <button onClick={resetBoard}>Reset game</button>
       <Board>
