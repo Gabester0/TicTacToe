e2188405bfd28d08562ae3320fbe16973a024dca
Gabester0 <gceipper@gmail.com>
2020-09-06 01:53:39 -0400

    properly initialized socket.io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
+require(`dotenv`).config();
+
 const express = require(`express`);
 const app = express();
-require(`dotenv`).config();
-const options = {}
-const io = require(`socket.io`)(app, options)
+const options = {};
+const http = require(`http`);
+const io = require(`socket.io`)(http);
 const port = process.env.PORT;
 
 app.get(`/`, (req, res)=>{
     res.send(`Hello world`)
+    io.on(`connection`, socket => {
+        console.log(`Socket Connection: ${socket}`)
+    })
 });
+
 
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`)
