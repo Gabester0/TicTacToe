a22044fcecf4212f835a61c01e7dc0d60e910a23
Gabester0 <gceipper@gmail.com>
2020-09-17 22:09:00 -0400

    added express-session, redis-connect, and redis

--- server/server.js
+++ server/server.js
@@ -2,11 +2,28 @@ require(`dotenv`).config();
 
 const app = require('express')();
 const server = require('http').createServer(app);
-const options = { /* ... */ }; //{ perMessageDeflate: false }
+
+//Import redis and express-session to set-up server data storage
+const redis = require('redis');
+const session = require('express-session');
+
+//configure options in initiate socket.io
+const options = {  }; //{ perMessageDeflate: false }
 const io = require('socket.io')(server, options);
+
+//Import connect-redis and initiate local data storage
+let RedisStore = require('connect-redis')(session);
+let redisClient = redis.createClient();
 
 //Set up Express to store game-state in redis cache via express-session
 //https://flaviocopes.com/express-sessions/#:~:text=That's%20what%20sessions%20are.,maintained%20by%20the%20Express%20team.&text=After%20this%20is%20done%2C%20all,routes%20are%20now%20using%20sessions.
+app.use(
+   session({
+      store: new RedisStore({client: redisClient}),
+      secret: 'catsRCool',
+      resave: false
+   })
+)
 
 io.on('connection', socket => { 
     console.log(`Socket Connected`, socket.id)
