ada310b3c0a1bd42a73729d182d2a4e4d057126e
Gabester0 <gceipper@gmail.com>
2020-10-11 02:06:31 -0400

    hitting a wall, need to change my approach

--- server/gameLogic/games.js
+++ server/gameLogic/games.js
@@ -2,34 +2,38 @@ const { RedisClient } = require("redis");
 
 const findGame = (redis, socketID)=>{
     redis.hgetall(`games`, (err, val)=>{
-        if(err) return console.log(`There was a problem getting existing games`, err)
+        if(err) return console.log(`There was a problem getting existing games, Ln. 5`, err)
         //If value from hgetall(`games`) === null there are no games so we create a new game with gameNumber 1
         if(val === null) return createGame(redis, socketID, 1)
         //Logic to sort existing games here
         //Get all games
 
-        redis.hgetall(`games`, (err, val)=>{
-            if(err) return console.log(`Error finding a game, please try again`)
-            //Get all keys returned and filter out 
             const keys = Object.keys(val).filter( key=> key.split(`.`)[1] === `status`)
-            console.log(keys, ` From line 11 of findGame`)
+            console.log(keys, ` From Ln. 15 of findGame`)
             let newKey = 0;
-            keys.map( (key, i)=>{
+            let added = false
+            keys.map( async (key, i)=>{
                 //check if key status is false ? then add connecting player as second player and return
-                redis.hmget(`games`, key, (err, val)=>{ 
-                    if(err) return console.log(`Error getting current games value`)
+                const viableGame = await redis.hmget(`games`, key, async (err, val)=>{       //?Does this need to be async?
+                    if(err) return console.log(`Error getting current games value.  From Ln. 20`)
                     if(val[0] === `false`){
-                        console.log(key, val[0], i, socketID, ` Line 22`) 
+                        added = true;
+                        console.log(key, val[0], i, socketID, ` From Ln. 22`) 
                         redis.hmset(`games`, key, true, `${i + 1}.p2`, socketID); // hmset() is command to set multiple values
-                        return redis.hmget(`games`, key, `${i + 1}.p1`, `${i + 1}.p2`, (err, val)=> console.log(val))
+                        return true
+                      //  redis.hmget(`games`, key, `${i + 1}.p1`, `${i + 1}.p2`, (err, val)=> console.log(val, ` From Ln. 24`))
                     }
                 })
+                console.log(viableGame, `${i} times the charm`)
+                if(viableGame) return true
                 //Track game numbers as loop through in variable outside of forEach
                 newKey = i + 2;
             })
-            //else call createGame and pass redis, socketID, variable with gameNumbers count + 1
-            console.log(newKey)
-            createGame(redis, socketID, newKey);
+            if(!added){
+                //If no existing games have only 1 player, need to create game
+                console.log(` newKey from Ln. 34 `, newKey)
+                createGame(redis, socketID, newKey);
+            }
 
             //! PROBLEM !//
             //When 1st 2 players connect simultaneously, 1st player triggers findGame which checks for existing game, finds none, line
@@ -40,22 +44,15 @@ const findGame = (redis, socketID)=>{
 
         //First existing game with `${gameNumber}` === false add current player is a game with only 1 player
             //Need to make sure checking games in order of creation (I don't think redis hashes ensure creation order is maintained)
-        
-        //If no existing games have only 1 player, need to create game
-
-        //Need to return gameNumber so Can be stored by clients
-    })
     //Need to return gameNumber so Can be stored by clients
 }
 
 const createGame = (redis, socketID, gameNumber)=>{
-    console.log(`Creating a Game (Not really yet)`)
-    console.log(`first player: `, socketID)
     //games hash contains all existing games.  Each existing game will have:
     // key: gameNumber, value: Boolean (representing if game ready or not)
     // key: gamenumber.p1, value: socketID of this players connection
     redis.hmset(`games`, `${gameNumber}.status`, false, `${gameNumber}.p1`, socketID, `${gameNumber}.p2`, ``); // hmset() is command to set multiple values
-    redis.hmget(`games`, `${gameNumber}.status`, `${gameNumber}.p1`, `${gameNumber}.p2`, (err, val)=> console.log(`Game #${gameNumber} created: `, val))
+    redis.hmget(`games`, `${gameNumber}.status`, `${gameNumber}.p1`, `${gameNumber}.p2`, (err, val)=> console.log(`Game #${gameNumber} created: `, val, ` From Ln. 56`))
 }
 
 
