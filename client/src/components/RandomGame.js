209f48f02513c16cbec75e1c3f9579d09d57be4e
Gabester0 <gceipper@gmail.com>
2020-10-19 02:31:05 -0400

    rudimentary UI to handle waiting for second player

--- client/src/components/RandomGame.js
+++ client/src/components/RandomGame.js
@@ -7,15 +7,24 @@ import { StaticDiv, StyledH5, Btn, Cannon } from '../AppStyles';
 import { delayFunction } from '../utility/utilities';
 
 const RandomGame = (props)=>{
+    const [connected, setConnected] = useState(false)
     const [ready, setReady] = useState(false);
     const [board, setBoard] = useState( { ...Array(9).fill(null) } ); //server
     const [socket] = useSocket('http://localhost:5005/', {autoConnect: false});
+
     useEffect( ()=>{
         socket.connect(); // socket = io.connect('http://localhost:5005/');
-        socket.on('connection', (socket)=> console.log(`Socket Connected!`, socket.connected) )
-        socket.on("join", ({note, game, player, status})=> console.log(`Server message: ${note}`, game, player, status) )
-    }, [])
-    
+        socket.on('connection', (socket)=>{
+            setConnected(true)
+            console.log(`Socket Connected!`, socket.connected)
+        })
+        socket.on("join", ({note, game, player, status})=>{
+            if(status) setReady(true)
+            console.log(`Server message: ${note}`, game, player, status)
+        })
+    }, [connected, setConnected, ready, setReady, socket])
+
+
     // useEffect(()=>{
     //     socket.emit(`click`, {clicked: lastMove, player: player, id: socket.id})
     // }, [lastMove])
@@ -34,10 +43,11 @@ const RandomGame = (props)=>{
                 </StyledH5>
             </StaticDiv>
             <Btn >Quit Game</Btn> {/*onClick={quitGame} */}
-            <Btn >Back to menu</Btn>{/*onClick={props.menu} */}
-            <Board handleClick={handleClick} board={board} />
+            <Btn onClick={props.menu} >Back to menu</Btn>
+            <h2>{!ready && `Waiting for second player`}</h2>
+            {ready && <Board handleClick={handleClick} board={board} />}
             <Cannon
-                // show={winner}
+                show={false} //winner
                 src={require('../static/StubbyCannon.png')} 
                 alt="confetti canon"
                 ref={confettiAnchorRef} />
