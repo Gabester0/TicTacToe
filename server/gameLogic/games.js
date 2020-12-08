5b5825a3b6b88626a82b0a340b8415b6392e0d5b
Gabester0 <gceipper@gmail.com>
2020-12-08 16:16:41 -0500

    Redis connects again

--- server/gameLogic/games.js
+++ server/gameLogic/games.js
@@ -1,4 +1,4 @@
-// const { RedisClient, RedisStore } = require('../redis/redis');
+const { redisClient } = require('../redis/redis');
 
 // games hash map is just going to be game numbers with the value false when the first socket connects
 // and becoming true when the second connects.
@@ -10,28 +10,28 @@
 //    IF client gets this message and the status is false, client is player X, client stores this information to local state 
 //    ELSE client stores to local state they are player O
 
-const findGame = async (socket, io, RedisClient)=>{
-   const games = await RedisClient.getAsync(`games`).then(res=> console.log(res) );
+const findGame = async (socket, io)=>{
+   const games = await redisClient.getAsync(`games`).then(res=> console.log(res) );
    //Handle Creating First Game
    if(games === null){
-      await RedisClient.setAsync('games', 1)
-      await RedisClient.setAsync(`1.status`, false) //Set A Game status of false because only one player has joined
+      await redisClient.setAsync('games', 1)
+      await redisClient.setAsync(`1.status`, false) //Set A Game status of false because only one player has joined
       socket.join(`1`)
       io.to(`1`).emit("join", {game: 1, player: `X`, status: false, note: `This is game #${1} and player X has joined the game.  Waiting for a second player`}) //Communicate to first client Game number and player (X)
    //Handle adding second player, or creating additional games
    }else {
       const latestGame = games.split('')[games.length - 1]
-      const latestGameStatus = await RedisClient.getAsync(`${latestGame}.status`)
+      const latestGameStatus = await redisClient.getAsync(`${latestGame}.status`)
       //Add Player to existing Game
       if(latestGameStatus === `false`){
-         await RedisClient.setAsync(`${latestGame}.status`, true)
+         await redisClient.setAsync(`${latestGame}.status`, true)
          socket.join(`${latestGame}`)
          io.to(`${latestGame}`).emit("join", {game: latestGame, player: `O`, status: true, note: `This is game #${latestGame} and player O has joined the game.  Ready to play`}) //Communicate to second player Game number and player (O)
       //Handle creating new games after the first new game
       } else {
          const newGame = parseInt(latestGame) + 1
-         await RedisClient.setAsync('games', `${games}${newGame}`)
-         await RedisClient.setAsync(`${newGame}.status`, false) //Set A Game status of false because only one player has joined
+         await redisClient.setAsync('games', `${games}${newGame}`)
+         await redisClient.setAsync(`${newGame}.status`, false) //Set A Game status of false because only one player has joined
          socket.join(`${newGame}`)
          io.to(`${newGame}`).emit("join", {game: newGame, player: `X`, status: false, note: `This is game #${newGame} and player X has joined the game.  Waiting for a second player`}) //Communicate to first client Game number and player (X)
       }
--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ require(`dotenv`).config();
 const app = require('express')();
 const server = require('http').createServer(app);
 
-/*Move*/  const redis = require('redis');
+/*Move*/  //const redis = require('redis');
 /*Copy?*/const session = require('express-session');
 const io = require('socket.io')(server);  //const options = {  }; //{ perMessageDeflate: false }
-/*Move?*/  let RedisStore = require('connect-redis')(session);
-/*Move?*/  let redisClient = redis.createClient();
-/*Move?*/  const bluebird = require('bluebird'); //Import Bluebird to make redis get/set-Async calls promises
-/*Move?*/  bluebird.promisifyAll(redis.RedisClient.prototype);
-// const { RedisClient, RedisStore } = require('./redis/redis');
+/*Move?*/  //let RedisStore = require('connect-redis')(session);
+/*Move?*/  //let redisClient = redis.createClient();
+/*Move?*/  //const bluebird = require('bluebird'); //Import Bluebird to make redis get/set-Async calls promises
+/*Move?*/  //bluebird.promisifyAll(redis.RedisClient.prototype);
+const { redisClient, RedisStore } = require('./redis/redis');
 
 const { initiateBoard } = require('./gameLogic/board');
 const { findGame } = require('./gameLogic/games');
@@ -26,7 +26,7 @@ io.on('connection', async (socket) => {
    console.log(`Socket Connected`, socket.id)
    // socket.emit("message", {note: "I am your server"})
 
-   findGame(socket, io, RedisClient)
+   findGame(socket, io)
 
    // initiateBoard(redisClient, socket.id);
    socket.on('click', socket=>{
